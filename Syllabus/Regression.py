30483c99548c11928a9d49ea734b8acc51b4a772
VarunReddyT <tvarun2014@gmail.com>
2025-07-17 11:04:41 +0530

    Syllabus

--- Syllabus/Regression.py
+++ Syllabus/Regression.py
@@ -1,18 +1,12 @@
 import numpy as np
 
-
 def mse(b, w, points):
-
     totalError = 0
-
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-
         totalError += (y - (w * x + b)) ** 2
-
         return totalError / float(len(points))
-
 
 def step_gradient(b_current, w_current, points, lr):
 
@@ -24,7 +18,6 @@ def step_gradient(b_current, w_current, points, lr):
         y = points[i, 1]
 
         b_gradient += (2 / M) * ((w_current * x + b_current) - y)
-
         w_gradient += (2 / M) * x * ((w_current * x + b_current) - y)
 
     new_b = b_current - (lr * b_gradient)
@@ -36,11 +29,9 @@ def gradient_descent(points, starting_b, starting_w, lr, num_iterations):
 
     b = starting_b
     w = starting_w
-
+    
     for step in range(num_iterations):
-
         b, w = step_gradient(b, w, np.array(points), lr)
-
         loss = mse(b, w, points)
         if step % 50 == 0:
             print(f"iteration:{step}, loss:{loss}, w:{w}, b:{b}")
